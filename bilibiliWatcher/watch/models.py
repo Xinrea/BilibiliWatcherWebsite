aab9054497e17d96cdc4b195dad8a064f7d95e54
Xinrea <s11s@vip.qq.com>
2018-06-11 19:31:17 +0800

    bind models to mysql server

--- bilibiliWatcher/watch/models.py
+++ bilibiliWatcher/watch/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+
+class Accounts(models.Model):
+    uid = models.AutoField(primary_key=True)
+    uname = models.CharField(max_length=30)
+    passwd = models.CharField(max_length=255)
+    rtime = models.DateField()
+
+    class Meta:
+        managed = False
+        db_table = 'accounts'
+
+
+class Cards(models.Model):
+    cardid = models.CharField(primary_key=True, max_length=128)
+    upid = models.ForeignKey('Upinfo', models.DO_NOTHING, db_column='upid')
+    title = models.CharField(max_length=255)
+    content = models.CharField(max_length=512)
+    ptime = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'cards'
+
+
+class Upinfo(models.Model):
+    upid = models.IntegerField(primary_key=True)
+    upname = models.CharField(max_length=64)
+    des = models.CharField(max_length=512, blank=True, null=True)
+    wtime = models.DateField()
+
+    class Meta:
+        managed = False
+        db_table = 'upinfo'
+
+
+class Usrinfo(models.Model):
+    uid = models.ForeignKey(Accounts, models.DO_NOTHING, db_column='uid', primary_key=True)
+    des = models.CharField(max_length=512, blank=True, null=True)
+    face = models.CharField(max_length=128, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'usrinfo'
+
+
+class Watch(models.Model):
+    uid = models.ForeignKey(Accounts, models.DO_NOTHING, db_column='uid', primary_key=True)
+    upid = models.ForeignKey(Upinfo, models.DO_NOTHING, db_column='upid', blank=True, null=True)
+    uwtime = models.DateField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'watch'

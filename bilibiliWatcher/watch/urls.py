e1f51a8dddc95daeaca8b64430c6f642404b7a18
Xinrea <s11s@vip.qq.com>
2018-06-13 18:08:20 +0800

    add login function

--- bilibiliWatcher/watch/urls.py
+++ bilibiliWatcher/watch/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('user/<slug:username>',views.manage,name='manage'),
     path('<int:id>',views.dynamic,name='dynamic'),
     path('all/',views.dynamic_all,name='dynamic_all'),
     path('video/',views.dynamic_video,name='dynamic_video'),
--- bilibiliWatcher/watch/views.py
+++ bilibiliWatcher/watch/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
-from watch.models import Cards,Upinfo
+from watch.models import Cards,Upinfo,Accounts
 from django.db.models import Count
 
 # Create your views here.
@@ -8,35 +8,65 @@ def index(request):
     upinfos = Upinfo.objects.all()
     cards = Cards.objects.all()
     tag = True
-    return render(request,'watch/home.html',{'upinfos':upinfos,'cards':cards,'tag':tag})
+    v = request.session.get('name')
+    if v:
+        usern = v
+        return render(request,'watch/home.html',{'upinfos':upinfos,'cards':cards,'tag':tag,'usern':usern})
+    else:
+        return render(request,'watch/home.html',{'upinfos':upinfos,'cards':cards,'tag':tag})
 
 def dynamic(request,id):
     up = Upinfo.objects.get(upid=int(id))
     cards = Cards.objects.filter(upid=int(id)).order_by("-ptime")
     type = 0
     tag = False
-    return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type})
+    v = request.session.get('name')
+    if v:
+        usern = v
+        return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type,'usern':usern})
+    else:
+        return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type})
+
+    
 
 def dynamic_all(request):
     cards = Cards.objects.order_by("-ptime")
     up = {}
     type = 1
     tag = False
-    return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type})
+    v = request.session.get('name')
+    if v:
+        usern = v
+        return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type,'usern':usern})
+    else:
+        return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type})
+    
 
 def dynamic_video(request):
     cards = Cards.objects.all()
     up = {}
     type = 2
     tag = False
-    return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type})
+    v = request.session.get('name')
+    if v:
+        usern = v
+        return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type,'usern':usern})
+    else:
+        return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type})
+    
 
 def dynamic_text(request):
     cards = Cards.objects.all()
     up = {}
     type = 3
     tag = False
-    return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type})
+    v = request.session.get('name')
+    if v:
+        usern = v
+        return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type,'usern':usern})
+    else:
+        return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type})
+    
 
 def statistic(request):
     ups = Upinfo.objects.all()
@@ -45,7 +75,32 @@ def statistic(request):
     for i in ups:
         upnames.append(i.upname)
         data.append(Cards.objects.filter(upid=i.upid).aggregate(Count('cardid'))['cardid__count'])
-    return render(request,'watch/statistic.html',{'upnames':upnames,'data':data})
+
+    v = request.session.get('name')
+    if v:
+        usern = v
+        return render(request,'watch/statistic.html',{'upnames':upnames,'data':data,'usern':usern})
+    else:
+        return render(request,'watch/statistic.html',{'upnames':upnames,'data':data})
+    
 
 def login(request):
-    return render(request,'watch/login.html')
+    if request.method == 'GET':
+        v = request.session.get('name')
+        if v:
+            usern = v
+            return render(request,'watch/login.html',{'usern':usern})
+        else:
+            return render(request,'watch/login.html')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('passwd')
+        account = Accounts.objects.filter(uname=username,passwd=password).first()
+        if account:
+            request.session['name']=account.uname
+            return redirect('/watch/')
+        else:
+            return render(request,'watch/login.html',{'error':True})
+
+def manage(request,username):
+    return HttpResponse(username)

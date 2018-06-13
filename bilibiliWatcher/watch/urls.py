65b54f37ecfdac7ce361c6c8f81c2422413c8b12
Xinrea <s11s@vip.qq.com>
2018-06-13 16:46:47 +0800

    update

--- bilibiliWatcher/watch/urls.py
+++ bilibiliWatcher/watch/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('<int:id>',views.dynamic,name='dynamic'),
+    path('all/',views.dynamic_all,name='dynamic_all'),
+    path('video/',views.dynamic_video,name='dynamic_video'),
+    path('text/',views.dynamic_text,name='dynamic_text'),
+    path('statistic/',views.statistic,name='statistic'),
+    path('login/',views.login,name='login'),
 ]
--- bilibiliWatcher/watch/views.py
+++ bilibiliWatcher/watch/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from watch.models import Cards,Upinfo
+from django.db.models import Count
 
 # Create your views here.
 def index(request):
@@ -11,6 +12,40 @@ def index(request):
 
 def dynamic(request,id):
     up = Upinfo.objects.get(upid=int(id))
-    cards = Cards.objects.filter(upid=int(id))
+    cards = Cards.objects.filter(upid=int(id)).order_by("-ptime")
+    type = 0
     tag = False
-    return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag})
+    return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type})
+
+def dynamic_all(request):
+    cards = Cards.objects.order_by("-ptime")
+    up = {}
+    type = 1
+    tag = False
+    return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type})
+
+def dynamic_video(request):
+    cards = Cards.objects.all()
+    up = {}
+    type = 2
+    tag = False
+    return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type})
+
+def dynamic_text(request):
+    cards = Cards.objects.all()
+    up = {}
+    type = 3
+    tag = False
+    return render(request,'watch/dynamic.html',{'up':up,'cards':cards,'tag':tag,'type':type})
+
+def statistic(request):
+    ups = Upinfo.objects.all()
+    upnames = []
+    data = []
+    for i in ups:
+        upnames.append(i.upname)
+        data.append(Cards.objects.filter(upid=i.upid).aggregate(Count('cardid'))['cardid__count'])
+    return render(request,'watch/statistic.html',{'upnames':upnames,'data':data})
+
+def login(request):
+    return render(request,'watch/login.html')
